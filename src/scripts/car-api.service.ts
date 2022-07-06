949cbb49e27024e54470b4b7bfbae42655b72098
Vahid Mohammadi <vahidm1467@gmail.com>
2022-07-06 17:31:44 +0430

    fix: ie

--- src/scripts/car-api.service.ts
+++ src/scripts/car-api.service.ts
@@ -3,8 +3,11 @@ import { CarConfig, CarDataResponse } from '../models/car-data.model';
 export class CarApiService {
   static async fetchCarData(car: CarConfig) {
     console.log('#ee car 5', car);
-    const response = await fetch(car.dataUrl);
-    console.log('#ee response 7', response, response.json(), await response.json());
-    return (await response.json()) as CarDataResponse[];
+    const response = await fetch(car.dataUrl, {
+      headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+    });
+    const json = await response.json();
+    console.log('#ee response 7', response, json);
+    return json as CarDataResponse[];
   }
 }
